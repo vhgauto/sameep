c99fa9e3ffac219df271d3b9f01d5b07710aa200
vhgauto <victor.gauto@outlook.com>
2022-11-25 14:39:58 -0300

    Agrego script para la descarga y extracción de reflectancias de superficie

---
+++ scripts/obtencion_datos_gis.R
@@ -0,0 +1,211 @@
+#!/usr/bin/env Rscript
+
+# librerías
+library(sf)
+library(sen2r)
+library(lubridate)
+library(glue)
+library(raster)
+library(rgdal)
+library(tidyverse)
+
+# día de la fecha
+hoy <- ymd(20221122) - 1 # ymd(20221102) # today() - 1
+
+# sitio de interés
+lr <- st_sfc(st_point(c(305789.86931, 6965069.94723)), crs = 32721)
+
+# función para generar mensajes en la consola, para separar las secciones
+f_msj <- function(x) {
+    a <- nchar(x)
+    b <- str_flatten(rep("X", a + 8))
+    c <- str_flatten(c("X", rep(" ", a + 6), "X"))
+    d <- str_flatten(c("X   ", x, "   X"))
+    e <- glue("\n\n{b}\n{c}\n{d}\n{c}\n{b}\n\n")
+
+    return(e)
+}
+
+# DESCARGA DE PRODUCTO
+
+print(glue("{f_msj('DESCARGA DE PRODUCTO')}"))
+
+# credenciales
+write_scihub_login('vhgauto', '6EVmlMfiDlrA7VzVBAEU') 
+
+# busco producto Sentinel-2 MSI L2A, tile 21JUK, vía 'scihub'
+lis <- s2_list(
+               spatial_extent = lr,
+               time_interval = c(hoy, hoy),
+               level = "L2A",
+               tile = "21JUK",
+               server = "scihub"
+               )
+
+# condición de ERROR
+if (length(lis) == 0) {
+   stop(glue("'\n\n\nNo hay producto disponible para el {hoy}.\n\n\n'"))
+}
+
+print(glue("\n\nDescargando producto {names(lis)}\n\n"))
+
+# creo la carpeta de descarga
+dir.create("safe")
+
+# descarga el producto
+s2_download(lis, service = "apihub", overwrite = FALSE,
+            outdir = "safe/")
+
+print(glue("\n\nProducto descargado\n\n"))
+
+# RECORTE DE PRODUCTO
+
+# conviene recortar el producto previa extracción,
+# para que el  resampling se lleve a cabo en una
+# imagen de menor tamaño (y no en la imagen entera)
+# (el resampling de toda la escena tarda UN MONTÓN)
+print(glue("{f_msj('RECORTE DE PRODUCTO')}"))
+
+# vector
+print(glue("\n\nCargo vertor de la región de inter\u00E9s\n\n"))
+vec <- shapefile("vectores/roi_LT.shp", verbose = FALSE)
+
+# solo me interesan R10m y R20m (R60m NO!)
+print(glue("\n\nCargo las bandas de inter\u00E9s\n\n"))
+
+reso <- list.files(file.path(list.files(file.path(getwd(), "safe",
+                   names(lis), "GRANULE"), full.names = TRUE), "IMG_DATA"),
+                   full.names = TRUE)
+
+# orden correcto de las bandas:
+# B01, B02, B03, B04, B05, B06, B07, B08, B8A, B11, B12 [11 elementos]
+lis_1020 <- c(
+    list.files(reso[3], full.names = T)[2],    # B01
+    list.files(reso[1], full.names = T)[2:4],  # B02, B03, B04
+    list.files(reso[2], full.names = T)[6:8],  # B05, B06, B07
+    list.files(reso[1], full.names = T)[5],    # B08
+    list.files(reso[2], full.names = T)[11],   # B8A
+    list.files(reso[2], full.names = T)[9:10]  # B11, B12
+)
+
+# lista que contiene las bandas ráster
+print(glue("\n\nGenero los r\u00E1ster para cada banda\n\n"))
+ras_1020 <- map(.x = lis_1020, raster)
+
+# lista que contiene el subset según el ROI
+print(glue("\n\nRecorto las bandas a la regi\u00F3n de inter\u00E9s\n\n"))
+subset <- map(.x = ras_1020, ~ crop(.x, vec))
+
+# resampling de los ráster a 10 m
+print(glue("\n\nResampling de bandas a 10 m\n\n"))
+# p/crear un stack que contenga todas las bandas,
+# tengo que cambiar la resolución de las bandas
+# bandas con 60m: B01 [1]
+# bandas con 20m: B05 [5], B06 [6], B07 [7] , B8A [9], B11 [10], B12 [11]
+# bandas con 10m: B02 [2], B03 [3], B04 [4], B08 [8]
+subset_B02 <- subset[[2]]
+subset_B01 <- resample(subset[[1]], subset[[2]], method = "ngb")
+subset_B03 <- subset[[3]]
+subset_B04 <- subset[[4]]
+subset_B05 <- resample(subset[[5]], subset[[2]], method = "ngb")
+subset_B06 <- resample(subset[[6]], subset[[2]], method = "ngb")
+subset_B07 <- resample(subset[[7]], subset[[2]], method = "ngb")
+subset_B08 <- subset[[8]]
+subset_B8A <- resample(subset[[9]], subset[[2]], method = "ngb")
+subset_B11 <- resample(subset[[10]], subset[[2]], method = "ngb")
+subset_B12 <- resample(subset[[11]], subset[[2]], method = "ngb")
+
+# creo el stack
+subset_stack <-
+    raster::stack(subset_B01, subset_B02, subset_B03,
+                    subset_B04, subset_B05, subset_B06,
+                    subset_B07, subset_B08, subset_B8A,
+                    subset_B11, subset_B12)
+
+names(subset_stack) <- c("B01", "B02", "B03", "B04", "B05", "B06",
+                         "B07", "B08", "B8A", "B11", "B12")
+
+# creo la carpeta del recorte
+# creo la carpeta de descarga
+dir.create("recortes")
+
+# escribir el stack
+print(glue("\n\nEscribo el stack de bandas recortado\n\n"))
+writeRaster(
+    subset_stack,
+    filename = "recortes/recorte.tif",
+    format = "GTiff",
+    overwrite = TRUE
+)
+
+# EXTRACCIÓN
+
+print(glue("{f_msj('EXTRACCIÓN DE REFLECTANCIAS')}"))
+
+# archivo stack
+print(glue("\n\nLevanto el stack subset\n\n"))
+ras1 <- "recortes/recorte.tif"
+
+# levanto el stack
+rast <- raster::stack(ras1)
+
+# cargo el vector de puntos muestrales
+print(glue("\n\nVector de puntos muestrales\n\n"))
+puntos <- shapefile("vectores/linea_puntos_LT.shp")
+
+# creo el data.frame con los datos de valor de pixel
+# nombre de las filas del data.frame
+nomb_row <- c("B01", "B02", "B03", "B04", "B05", "B06",
+              "B07", "B08", "B8A", "B11", "B12")
+
+# extraigo los valores de reflectancia de superficie del ráster
+print(glue("\n\nExtraigo los valores de p\u00EDxel\n\n"))
+base <- raster::extract(rast, puntos)
+
+# canvierto a data.frame y agrego columna con los puntos
+base <- data.frame(base, punto = c("LR1", "LR2", "LR3", "LT"))
+
+# arreglo los datos
+base <- base |>
+        pivot_longer(cols = -punto,
+                     values_to = "firma",
+                     names_to = "param") |>
+        pivot_wider(id_cols = param,
+                    values_from = firma,
+                    names_from = punto) |>
+        # cambio el nombre de las bandas
+        mutate(param = nomb_row) |>
+        # factor de escala
+        mutate(LR1 = LR1 / 10000,
+               LR2 = LR2 / 10000,
+               LR3 = LR3 / 10000,
+               LT = LT / 10000) |>
+        # agrego la fecha dada
+        mutate(fecha = ymd(hoy)) |>
+        # reacomodo el orden de las columnas
+        dplyr::select(fecha, param, LR1, LR2, LR3, LT)
+
+# escribo los datos nuevos
+write_tsv(base, file = "datos/datos_nuevos.tsv")
+
+# leo base de datos
+base_de_datos <- read_tsv("datos/base_de_datos.tsv")
+
+# combino con la base de datos
+print(glue("\n\nIncorporo a la base de datos\n\n"))
+base_de_datos <- bind_rows(base_de_datos, base)
+
+# sobreescrivo el archivo .tsv
+write_tsv(base_de_datos, file = "datos/base_de_datos.tsv")
+
+# muestro la tabla en la consola
+base
+
+# elimino el SAFE y el recorte
+print(glue("\n\nElimino recorte\n\n"))
+unlink(list.files(path = "safe", full.names = TRUE), recursive = TRUE)
+unlink(list.files(path = "recortes", full.names = TRUE), recursive = TRUE)
+
+# FIN DEL PROCESO
+
+print(glue("{f_msj('PROCESO FINALIZADO')}"))
